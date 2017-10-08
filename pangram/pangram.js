f25066234cbb6de97a78aa538f45669ea304f2e1
Cyril Dolisy <dolisy.cyril@gmail.com>
2017-10-08 23:08:11 +0200

    new iteration pangram

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,8 +1,5 @@
 let Pangram = function (string) {
   this.string = string;
-  // if (!string.match(/^[a-z]+$/)) {
-  //   throw new Error("Bad string");
-  // };
 };
 
 module.exports = Pangram;
@@ -10,14 +7,25 @@ module.exports = Pangram;
 Pangram.prototype.isPangram = function () {
   let string = this.string;
   let alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  let check = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
-  for (let i = 0; i < string.length; i++) {
-    if (alphabet.includes(string[i].toLowerCase()) && check[alphabet.indexOf(string[i].toLowerCase())] === 0) {
-      check[alphabet.indexOf(string[i].toLowerCase())] = 1;
+  let sum = 0;
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (string.toLowerCase().includes(alphabet[i])) {
+      sum += 1;
     }
   }
 
-  let sum = check.reduce(function(a, b) { return a + b; }, 0);
-  return sum === 26;
+  return sum === alphabet.length;
+
+  // let check = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+  // for (let i = 0; i < string.length; i++) {
+  //   if (alphabet.includes(string[i].toLowerCase()) && check[alphabet.indexOf(string[i].toLowerCase())] === 0) {
+  //     check[alphabet.indexOf(string[i].toLowerCase())] = 1;
+  //   }
+  // }
+
+  // let sum = check.reduce(function(a, b) { return a + b; }, 0);
+  // return sum === 26;
 };
